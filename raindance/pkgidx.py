45db79bf66da228168f60c4a5e50045cefc961d5
Whi Morriss <whit.morriss@canonical.com>
2014-06-10 17:24:49 -0500

    implementation for downloading dependencies

--- raindance/pkgidx.py
+++ raindance/pkgidx.py
@@ -1,12 +1,69 @@
 from boto.exception import S3ResponseError
+#from contextlib import closing
 from path import path
+import gevent
+import gevent.monkey
 import json
 import logging
-import s3po
 import operator
+import grequests
+import s3po
+import sys
 
+gevent.monkey.patch_all()
 
 logger = logging.getLogger(__name__)
+
+
+def deps_command(ctx, pargs):
+    release = ctx['release']
+    job = release.query_job(pargs.name)
+    assert job, "Job not found"
+    dd = pargs.download_dir.expanduser()
+    dd.makedirs_p()
+
+    rsession = grequests.Session()
+    resp = rsession.get(pargs.index)
+
+    assert resp.ok, "Issue with connecting to %s: %s" %(pargs.index, resp)
+    data = resp.json()
+
+    dep_data = [(pkg, data[pkg]) for pkg in job.packages]
+    greenlets = []
+    for name, data in dep_data:
+        url = '/'.join((pargs.index, data['filename']))
+        g = gevent.spawn(download, rsession, url, data['filename'],
+                         data['sha1'], dd)
+        greenlets.append(g)
+    gevent.wait(greenlets)
+
+
+def download(session, url, filename, sha1, outputdir):
+    dest = outputdir / filename
+    worked = False
+    if not dest.exists():
+        resp = session.get(url, stream=True)
+        if resp.ok:
+            dest.write_text(resp.content)
+        assert dest.read_hexhash('sha1') == sha1
+        worked = True
+    return worked, dest
+
+
+
+
+        # #with closing(session.get(url, stream=True)) as resp, fp:
+        # session.get(url, stream=True)
+        # with fp:
+        #     written = 0
+        #     for chunk in resp.iter_content():
+        #         fp.write(chunk)
+        #         written += len(chunk)
+        #         gevent.sleep(0)
+        #         if written == resp.headers['content-length']:
+        #             break
+
+
 
 
 def upload_compiled_packages(ctx, pargs):
