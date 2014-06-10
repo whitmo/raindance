c457bf86beb3626354baa03c4c091d01277c2241
Whi Morriss <whit.morriss@canonical.com>
2014-06-10 09:17:41 -0500

    erb extraction

--- raindance/job.py
+++ raindance/job.py
@@ -1,14 +1,30 @@
+from .release import Job
+from functools import partial
 import json
 
 
 def job_command(ctx, pargs):
     pass
 
+jdump = partial(json.dumps, indent=2)
+
 
 def print_jobs(ctx, pargs):
     release = ctx['release']
     joblist = sorted([str(x.basename()) for x in release.jobs.dirs()])
-    print(json.dumps(joblist, indent=2))
+    print(jdump(joblist))
+    return 0
+
+
+def print_erb_tags(ctx, pargs):
+    release = ctx['release']
+    if pargs.job == 'all':
+        jobs = [Job(jpath) for jpath in sorted(release.jobs.dirs())]
+    else:
+        jobs = [release.query_job(pargs.job)]
+    allvars = {str(job.basename()): [x for x in job.erb_vars] for job in jobs}
+    print(jdump(allvars))
+    return 0
 
 
 def print_spec(ctx, pargs):
@@ -19,4 +35,4 @@ def print_spec(ctx, pargs):
     filter_by = [x for x in attrs if getattr(pargs, x)]
     if any(filter_by):
         spec = {attr:spec[attr] for attr in filter_by}
-    print(json.dumps(spec, indent=2))
+    print(jdump(spec))
--- raindance/release.py
+++ raindance/release.py
@@ -2,20 +2,32 @@ from functools import partial
 from util import filepath
 import path
 import util
-
+import re
 
 spec_val = partial(util.submap_value, 'spec')
 
 
 class Job(path.path):
     spec = filepath('spec', util.load_yaml)
+    packages = spec_val('packages')
+    properties = spec_val('properties')
+    template_map = spec_val('templates')
+
+    match_erb = re.compile('.*<\%(.*)\%>.*', re.MULTILINE)
+
     monit = filepath('monit')
     templates = filepath('templates')
 
-    name = spec_val('name')
-    packages = spec_val('packages')
-    properties = spec_val('properties')
-    templates = spec_val('templates')
+    @property
+    def erb_vars(self):
+        files = (self / 'templates').files()
+        for tmplt in files:
+            name = str(tmplt.basename())
+            text = tmplt.text()
+            match_iter = self.match_erb.finditer(text)
+            out = [x.groups()[0].strip() for x in match_iter]
+            if len(out):
+                yield name, out
 
 
 class Release(path.path):
