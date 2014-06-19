d68fbcad0e83d9c20c658d0e4e72fcd360e67833
caleb miles <cmiles@pivotallabs.com>
2014-06-19 13:33:38 -0700

    implementation for new commands and tests

---
+++ raindance/pipeline.py
@@ -0,0 +1,146 @@
+from boto.exception import S3ResponseError
+from path import path
+from . import util
+import gevent
+import gevent.monkey
+import logging
+import s3po
+import tarfile
+import yaml
+
+gevent.monkey.patch_all()
+
+logger = logging.getLogger(__name__)
+
+
+class UploadJobArtefacts(object):
+    manifest = 'compiled_packages.MF'
+    def __init__(self, workdir, release, logger=logger):
+        self.release = release
+        self.manifest = workdir / self.manifest
+        self.blobs = workdir / 'compiled_packages/blobs'
+        self.outdir = workdir / 'artefacts'
+        self.workdir = workdir
+        self.log = logger
+
+    @property
+    def manifest_data(self):
+        return util.load_yaml(self.manifest)
+
+    def setup(self):
+        if not self.outdir.exists():
+            self.outdir.makedirs_p()
+
+    def extract_packages(self, exported_packages, workdir):
+        tgz = tarfile.open(exported_packages, 'r:gz')
+        tgz.extractall(workdir)
+
+    def dependency_data(self, packages, export_data):
+        for package in packages:
+            out = export_data.get(package, (None, None))
+            if all(out):
+                yield package, out
+            else:
+                yield (package, (False, False))
+
+    def copy_packages(self, jobtmp, depdata):
+        packages = jobtmp / 'packages'
+        packages.mkdir_p()
+        for pkg, (sha1, blobid) in depdata:
+            if blobid is False:
+                yield False, pkg
+                break
+
+            blob = self.blobs / blobid
+            self.log.debug("%s %s", blob.basename(), blob.exists())
+
+            self.verify_file(blob, sha1)
+
+            dest = packages / '%s-%s' %(pkg, sha1)
+            blob.copy(dest)
+            yield True, pkg
+
+    def populate_job_templates(self):
+        export_data = util.packages_from_manifest(self.manifest_data)
+        jobdata = ((job, job.packages) for job in self.release.joblist)
+
+        for job, packages in jobdata:
+            tmp = self.workdir / job.basename()
+            job.copytree(tmp)
+            depdata = self.dependency_data(packages, export_data)
+            for status, pkg in self.copy_packages(tmp, depdata):
+                if status is False:
+                    self.log.error('ATT: No pkg %s for %s', pkg, job.basename())
+            yield tmp
+
+    def verify_file(self, path, sha1):
+        assert path.read_hexhash('sha1') == sha1, "sha mismatch: %s" % path
+
+    def tarzip_jobs(self, template_dirs):
+        for dir in template_dirs:
+            dest = self.outdir / dir.basename()
+            with util.pushd(dir), tarfile.open(dest, 'w:gz') as tgz:
+                tgz.add('.')
+            yield dest
+
+    @classmethod
+    def command(cls, ctx, pargs):
+        logger.info(pargs.workdir)
+        release = ctx['release']
+
+        uja = cls(pargs.workdir, release)
+        uja.setup()
+
+        if not uja.manifest.exists():
+            uja.extract_packages(pargs.exported_packages, pargs.workdir)
+
+        tds = uja.populate_job_templates()
+
+        for tarball in uja.tarzip_jobs(tds):
+            print(tarball)
+
+upload_job_artefacts = UploadJobArtefacts.command
+
+
+class UploadExport(object):
+    name_tmplt = "{release_name}-{release_version}-{release_commit_hash}-amd64.tar.gz"
+
+    def __init__(self, filepath, bucket, s3cxn):
+        self.bucket = bucket
+        self.filepath = path(filepath).abspath()
+        self.s3 = s3cxn
+
+    def extract_manifest_data(self):
+        tgz = tarfile.open(self.filepath, 'r:gz')
+        fp = tgz.extractfile('compiled_packages.MF')
+        data = yaml.load(fp)
+        return data
+
+    def upload(self, release_data):
+        key = self.name_tmplt.format(**release_data)
+        with open(self.filepath) as stream:
+            self.s3.upload(self.bucket, key, stream, headers={'Content-Type':'application/x-tar'}, retries=3)
+
+    @classmethod
+    def command(cls, ctx, pargs):
+        creds = pargs.access_key, pargs.secret_key
+
+        assert all(creds), "Missing AWS Credentials: a: '%s', s:'%s'" % creds
+
+        s3 = s3po.Connection(*creds)
+        try:
+            s3.conn.get_bucket(pargs.bucket)
+        except S3ResponseError:
+            s3.conn.create_bucket(pargs.bucket)
+
+        uploader = cls(pargs.tarball, pargs.bucket, s3)
+        data = uploader.extract_manifest_data()
+        uploader.upload(data)
+        return 0
+
+
+upload_export = UploadExport.command
+
+
+
+    
---
+++ raindance/tests/test_export_upload.py
@@ -0,0 +1,93 @@
+from itertools import count
+from mock import Mock
+from path import path
+from raindance.util import pushd
+import tarfile
+
+
+class TestUploadExport(object):
+    here = path(__file__).parent
+    def makeone(self, fp=here / 'dummy/dummy-compiled-packages.tgz'):
+        from raindance.pipeline import UploadExport
+        return UploadExport(fp, 'bucket', Mock(name='s3po'))
+
+    def test_extract_manifest_data(self):
+        ue = self.makeone()
+        data = ue.extract_manifest_data()
+        assert 'release_commit_hash' in data
+
+    def test_upload_to_s3(self):
+        ue = self.makeone()
+        ue.s3 = Mock(name='s3')
+        data = ue.extract_manifest_data()
+        ue.upload(data)
+        assert ue.s3.upload.called
+        assert ue.s3.upload.call_args[0][1] == 'dummy-0+dev.3-9364d68b-amd64.tar.gz'
+
+# def test_upload_command():
+#     from raindance.pipeline import UploadExport
+    
+#     with patch('raindance.pipeline.UploadExport.upload'), patch('raindance.pipeline.UploadExport.extract_manifest_data'):
+#         assert UploadExport.command(Mock(name='ctx'), Mock(name='pargs')) == 0
+
+
+class TestUploadJobArtefacts(object):
+    here = path(__file__).parent
+    counter = count()
+    sandbox = here / __name__
+    manifest_template = here / 'index.yml'
+    dummy = here /  'dummy'
+    dummy_cpc = dummy / 'dummy-compiled-packages.tgz'
+    dummy_release = dummy / 'release'
+    
+
+    def makeone(self, release=None):
+        from raindance.pipeline import UploadJobArtefacts
+        from raindance.release import Release
+        self.release = release or Mock(name='release', spec=Release)
+        return UploadJobArtefacts(self.sandbox, self.release)
+
+    def setup(self):
+        self.sandbox.rmtree_p()
+        self.sandbox.mkdir()
+        with tarfile.open(self.dummy_cpc, 'r:gz') as tgz, pushd(self.sandbox):
+            tgz.extractall()
+
+    def teardown(self):
+        self.sandbox.rmtree_p()
+        
+    def test_instantiation(self):
+        assert self.makeone()
+
+    def test_setup(self):
+        uja = self.makeone()
+        uja.setup()
+        assert (self.sandbox / 'artefacts').exists()
+
+    def test_populate_template_pkg_placement(self):
+        from raindance.release import Release
+
+        uja = self.makeone(release=Release(self.dummy_release))
+        uja.setup()
+        uja.extract_packages(self.dummy_cpc, uja.workdir)
+        uja.log = Mock(name='logger')
+        out = next(x for x in uja.populate_job_templates() if x.endswith('dummy_with_package'))
+        assert set([str(x.basename()) for x in out.files()]) == set(('monit', 'spec'))
+        assert set([str(x.basename()) for x in out.dirs()]) == set(('templates', 'packages'))
+        assert (out / 'packages/dummy_package-6b02ba72f6d0285a65166048b2e4522d7c126f7f').exists()
+
+    def test_populate_template_bad_package(self):
+        from raindance.release import Release
+
+        uja = self.makeone(release=Release(self.dummy_release))
+        uja.setup()
+        uja.extract_packages(self.dummy_cpc, uja.workdir)
+        uja.log = Mock(name='logger')
+        out = [x for x in uja.populate_job_templates()]
+        assert uja.log.error.called
+        assert 'bad_package' in uja.log.error.call_args[0]
+
+
+    
+        
+    
