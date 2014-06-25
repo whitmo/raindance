9d933d9558bcaabf1c37606f80b91b42946be905
Whi Morriss <whit.morriss@canonical.com>
2014-06-25 16:35:42 -0500

    add venv init and remove old file

--- helpers/ec2-setup/bosh-boilerplate-src.sh
+++ helpers/ec2-setup/bosh-boilerplate-src.sh
@@ -6,3 +6,5 @@ export CF_RELEASE_DIR=/opt/cf-release
 export BOSH_USER=admin
 export BOSH_PASSWORD=admin
 . /opt/packages/bin/activate
+export AWS_ACCESS_KEY_ID={{aaki}}
+export AWS_SECRET_ACCESS_KEY={{asak}}
