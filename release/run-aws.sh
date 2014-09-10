ccc11f644f8db0896f1e5dfd802b3515a87d28d9
Whi Morriss <whit.morriss@canonical.com>
2014-09-10 16:53:22 -0600

    less verbose

--- release/run-aws.sh
+++ release/run-aws.sh
@@ -16,7 +16,7 @@ aaki: ${AWS_ACCESS_KEY}
 asak: ${AWS_SECRET_ACCESS_KEY}
 EOF
 
-CMD="ansible-playbook -vvvv -i ./inventory.ini  ./aws.yml"
+CMD="ansible-playbook -vv -i ./inventory.ini  ./aws.yml"
 
 echo $CMD
 $CMD
