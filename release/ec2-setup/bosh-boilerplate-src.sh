fe30d3807c34da7a7dc9dfc4ce8cb161742268a4
Whi Morriss <whit.morriss@canonical.com>
2014-09-10 16:52:49 -0600

    fix breakage from new boshlite
     - install bosh gem
     - remove config setting

--- release/ec2-setup/bosh-boilerplate-src.sh
+++ release/ec2-setup/bosh-boilerplate-src.sh
@@ -1,7 +1,3 @@
-export RBENV_SHELL="bash"
-export RBENV_ROOT="/opt/rbenv"
-export PATH=/opt/rbenv/shims:/opt/rbenv/bin:/opt/rbenv/bin/rbenv:$PATH
-eval "$(rbenv init -)"
 export CF_RELEASE_DIR=/opt/cf-release
 export BOSH_USER=admin
 export BOSH_PASSWORD=admin
