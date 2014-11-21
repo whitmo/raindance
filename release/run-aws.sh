137490d730808a9eed12fde3fd793712794da8d9
Whit Morriss <whit.morriss@canonical.com>
2014-11-21 13:13:43 -0600

    add code for passing instance id

--- release/run-aws.sh
+++ release/run-aws.sh
@@ -14,6 +14,7 @@ cat > ./vars/run-vars.yml <<EOF
 rel: $1
 aaki: ${AWS_ACCESS_KEY}
 asak: ${AWS_SECRET_ACCESS_KEY}
+instance_id: ${2}
 EOF
 
 CMD="ansible-playbook -vv -i ./inventory.ini  ./aws.yml"
