0515814bad38d1aa1cf2857fbd7010cefaa22e89
DWM <whit.morriss@canonical.com>
2014-11-24 21:58:24 +0000

    add appropriate script for running from juju charm

---
+++ release/run-aws-remote.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+cat > ./inventory.ini <<EOF
+[local]
+localhost
+
+[blbox]
+
+EOF
+
+cat > ./vars/run-vars.yml <<EOF
+---
+rel: $1
+aaki: ${AWS_ACCESS_KEY}
+asak: ${AWS_SECRET_KEY}
+instance_id: ${2}
+EOF
+
+CMD="ansible-playbook --private-key=/home/ubuntu/.juju/ssh/juju_id_rsa -vv -i ./inventory.ini  ./aws.yml"
+
+echo $CMD
+$CMD
